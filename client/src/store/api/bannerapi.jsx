3b8565cbbd49e9964100628ed52577eced2a2fa2
karankumaryadav <kkcgamer865@gmail.com>
2024-12-22 23:33:42 +0530

    localhost correction

--- client/src/store/api/bannerapi.jsx
+++ client/src/store/api/bannerapi.jsx
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 export const bannerApi = createApi({
   reducerPath: "bannerApi",
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
